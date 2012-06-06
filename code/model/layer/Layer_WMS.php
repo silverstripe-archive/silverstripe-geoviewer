03c0aa30e692bab0f57660814911b4e6b623154a
Rainer Spittel <fb3rasp@gmail.com>
2012-06-06 17:55:15 +1200

    ENHANCEMENT: add WMS base class

---
+++ code/model/layer/Layer_WMS.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package geoviewer
+ * @subpackage model
+ * @author Rainer Spittel (rainer at silverstripe dot com)
+ *
+ */
+
+class Layer_WMS extends Layer {
+
+	static $db = array(
+		'Basemap' => 'Boolean',  
+	);
+	
+	function getJavaScript() {
+		throw new Layer_WMS_Exception('getJavaScript not implemented');
+	}
+	
+}
+
+class Layer_WMS_Exception extends Exception {
+	
+}
