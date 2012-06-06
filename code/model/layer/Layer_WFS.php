6ed72272ca455f183d051662bdcd59a1a4e78230
Rainer Spittel <fb3rasp@gmail.com>
2012-06-06 17:55:40 +1200

    ENHANCEMENT: add vector data clustering for WFS layers.

--- code/model/layer/Layer_WFS.php
+++ code/model/layer/Layer_WFS.php
@@ -9,16 +9,17 @@
 class Layer_WFS extends Layer {
 
 	static $db = array(
-		'Namespace' => 'Varchar',  // tiger
-		'FeatureType' => 'Varchar',  // poi
-		'Projection' => 'Varchar', // EPSG:4326
-		'Version' => 'Varchar',    // 1.1.0
+		'Namespace' => 'Varchar',  			// tiger
+		'FeatureType' => 'Varchar',  		// poi
+		'Projection' => 'Varchar', 			// EPSG:4326
+		'Version' => 'Varchar',    			// 1.1.0
+		'ClusterFeatures' => 'Boolean', 	// true/false
+		'ClusterDistance' => 'Int', 	// Integer value
 	);
 	
 	function getJavaScript() {
 		throw new Layer_WFS_Exception('getJavaScript not implemented');
 	}
-	
 }
 
 class Layer_WFS_Exception extends Exception {
