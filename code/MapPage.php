041f301f97ee1e58db617631ec70fda3f1c2dd6e
Rainer Spittel <fb3rasp@gmail.com>
2012-07-08 20:07:57 +1200

    MINOR refactor categorie cache handler and deprecated code.

--- code/MapPage.php
+++ code/MapPage.php
@@ -39,64 +39,47 @@ class MapPage_Controller extends Page_Controller {
 	 */
 	public function init() {
 		parent::init();
-		$this->extend('extendInit');
 	}
 
-	/**
-	 */
-	function getCategoriesByLayerType($layertype) {
-		$map = $this->dataRecord->MapObject();
-		$categories = $map->getCategories();
-		$retValue = new ArrayList();
+	// /**
+	//  */
+	// function getCategoriesByLayerType($layertype) {
+	// 	$map = $this->dataRecord->MapObject();
+	// 	$categories = $map->getCategories();
+	// 	$retValue = new ArrayList();
 
-		if($categories) {
-			foreach($categories as $category) {
- 				$layers = $category->getEnabledLayers($map, $layertype);
-				if ($layers->Count()) {
-					$entry = new ArrayData( array(
-						'Category' => $category,
-						'Layers' => $layers
-					));
-					$retValue->push($entry);
-				}
-			}
-		}
-		return $retValue;
-	}
+	// 	if($categories) {
+	// 		foreach($categories as $category) {
+ // 				$layers = $category->getEnabledLayers($map, $layertype);
+	// 			if ($layers->Count()) {
+	// 				$entry = new ArrayData( array(
+	// 					'Category' => $category,
+	// 					'Layers' => $layers
+	// 				));
+	// 				$retValue->push($entry);
+	// 			}
+	// 		}
+	// 	}
+	// 	return $retValue;
+	// }
 
-	/**
-	 */
-	function getOverlayCategories() {
-		return $this->getCategoriesByLayerType('overlay');
-	}
+	// /**
+	//  */
+	// function getOverlayCategories() {
+	// 	return $this->getCategoriesByLayerType('overlay');
+	// }
 
-	/**
-	 */
-	function getBackgroundCategories() {
-		return $this->getCategoriesByLayerType('background');
-	}	
+	// /**
+	//  */
+	// function getBackgroundCategories() {
+	// 	return $this->getCategoriesByLayerType('background');
+	// }	
 	
-	/**
-	 */
-	function getContextualCategories() {
-		return $this->getCategoriesByLayerType('contextual');
-	}
+	// /**
+	//  */
+	// function getContextualCategories() {
+	// 	return $this->getCategoriesByLayerType('contextual');
+	// }
 	
-	/**
-	 * Partial caching key. This should include any changes that would influence 
-	 * the rendering of LayerList.ss
-	 * 
-	 * @return String
-	 */
-	function CategoriesCacheKey() {
-		$LayerCategory = new DataList("LayerCategory");
-		$layer = new DataList("Layer");
-		return implode('-', array(
-			$this->MapObject()->ID, 
-			$LayerCategory->Max("LastEdited"),
-			$layer->Max("LastEdited"),
-			$this->request->getVar('layers')
-		));
-	}
 }
 
