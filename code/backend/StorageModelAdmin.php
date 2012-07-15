b19b136a2431fcef7a901115bb637fa7d4adfdee
Rainer Spittel <fb3rasp@gmail.com>
2012-07-15 22:23:25 +1200

    ENHANCEMENT add feature type import action for Storage management.

--- code/backend/StorageModelAdmin.php
+++ code/backend/StorageModelAdmin.php
@@ -24,5 +24,33 @@ class StorageModelAdmin extends ModelAdmin {
 	);
 
 	static $allowed_actions = array(
+		'doImportFeatureTypes'
 	);
+
+
+/*
+http://www.geoviewer.umwelt.bremen.de:8080/geoserver/wfs?service=wfs&version=1.1.0&request=getcapabilities
+*/
+	public function doImportFeatureTypes($request) {
+		$params = $request->getVars();
+
+		$ID = $params['ID'];
+		$storage = DataObject::get_by_id('StorageGeoserver',$ID);
+		if ($storage == false) {
+			$this->response->addHeader('X-Status', "Storage not known to the system.");			
+			return;
+		}
+
+		$data = array(
+			'Storage' => $storage
+		);
+		
+		// request all WFS FeatureTypes
+		$model = new GeoserverModel();
+		$model->setStorage($storage);
+
+		$result = $model->getWFSCapabilities();
+		$message = sprintf("In total %d feature types have been created.",$result);
+		$this->response->addHeader('X-Status', $message);
+	}
 }
